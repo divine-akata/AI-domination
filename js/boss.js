2a481da405838f65dd27efd5b6855e7ce367db49
Shreya Sethi <sethishreya02@gmail.com>
2026-02-14 11:48:13 -0700

    Add AI boss logic and rain system

--- js/boss.js
+++ js/boss.js
@@ -1,1 +1,42 @@
+export class Boss {
+  constructor() {
+    this.maxHP = 5;
+    this.hp = 5;
+    this.isAlive = true;
+    this.hasFled = false;
+    this.currentMove = "neutral";
+    this.isDefeated = false;
+  }
 
+  takeDamage(amount) {
+    if (!this.isAlive) return;
+
+    this.hp -= amount;
+
+    if (this.hp <= 0) {
+      this.enterDefeatStance();
+    }
+  }
+
+  enterDefeatStance() {
+    this.isDefeated = true;
+    this.currentMove = "handsUp";
+    console.log("🤖 Robot lifts hands in surrender.");
+  }
+
+  collapse() {
+    this.isAlive = false;
+    console.log("☠️ Robot collapses.");
+  }
+
+  flee() {
+    this.isAlive = false;
+    this.hasFled = true;
+    this.currentMove = "cry";
+    console.log("😭 Robot cries and runs away.");
+  }
+
+  setMove(move) {
+    this.currentMove = move;
+  }
+}
